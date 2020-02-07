b52c357ec5dc8965d9382b8cb6ab88e5e90d4197
davidtays <davidtays22@gmail.com>
2020-02-06 19:47:32 -0600

    Updates

---
+++ precache-manifest.bb4191c0d5fcbf24c93c47dd7161d5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a2d45baff234b984beec88cbdd085b",
+    "url": "/trying-react/index.html"
+  },
+  {
+    "revision": "e2a0a6dd0ac0fd0b0b4c",
+    "url": "/trying-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e2a0a6dd0ac0fd0b0b4c",
+    "url": "/trying-react/static/js/2.d97ae62f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/trying-react/static/js/2.d97ae62f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f407568909b6c86cc4a5",
+    "url": "/trying-react/static/js/main.8c67f159.chunk.js"
+  },
+  {
+    "revision": "5981f5bc34271d7cf7b7",
+    "url": "/trying-react/static/js/runtime-main.01d5bace.js"
+  }
+]);
